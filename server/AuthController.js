f087f375d059adecc14d441aed658176758a4840
Mohamed <mabdel8@students.towson.edu>
2024-05-11 22:49:47 -0400

    testing

--- server/AuthController.js
+++ server/AuthController.js
@@ -13,7 +13,8 @@ module.exports.Signup = async (req, res, next) => {
     const token = createSecretToken(user._id);
     res.cookie("token", token, {
       withCredentials: true,
-      httpOnly: false,
+      httpOnly: true,
+      sameSite: 'none'
     });
     res
       .status(201)
@@ -43,6 +44,7 @@ module.exports.Login = async (req, res, next) => {
          withCredentials: true,
          httpOnly: true,
          secure: true,
+         sameSite: 'none'
        });
        res.status(201).json({ message: "User logged in successfully", success: true });
        next()
